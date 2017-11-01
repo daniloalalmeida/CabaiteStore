eb57529a0451fc4162a945383453d6aa405c1a5f
daniloalalmeida <daniloalalmeida@gmail.com>
2017-11-01 20:26:49 -0200

    Update LojaCabaitePOO.sql

--- LojaCabaitePOO.sql
+++ LojaCabaitePOO.sql
@@ -16,10 +16,9 @@ CREATE	TABLE produto
      				preco_produto decimal(6,2) NOT null);			
      
 CREATE TABLE itens_pedido
-	(itens_pedido integer(6) PRIMARY KEY AUTO_INCREMENT,
-     		quantidade decimal(6,2) NOT null UNSIGNED,
-	 		CONSTRAINT FK_id_produto FOREIGN KEY (id_produto) 								
-				REFERENCES produto (id_produto));
+	(itens_pedido integer(6) PRIMARY KEY AUTO_INCREMENT, 
+	 	quantidade decimal(6,2) unsigned NOT null, 
+	 		id_produto integer(6), CONSTRAINT FK_id_produto FOREIGN KEY (id_produto) REFERENCES produto (id_produto));
      
 CREATE TABLE pedido
      (id_pedido integer(6) PRIMARY KEY,
@@ -31,5 +30,18 @@ CREATE TABLE pedido
 					REFERENCES pessoa (id_pessoa),
      						CONSTRAINT FK_itens_pedido FOREIGN KEY (itens_pedido) 								
 							REFERENCES itens_pedido (itens_pedido));
-      							
+
+#DADOS
+
+INSERT INTO `pessoa` (`id_pessoa`, `nome`, `endereco`, `cpf`, `telefone`, `sexo`) VALUES ('1', 'Julio', 'rua de barro', '123', '3223', 'M');
+INSERT INTO `pessoa` (`id_pessoa`, `nome`, `endereco`, `cpf`, `telefone`, `sexo`) VALUES ('2', 'Patrick', 'rua de barro', '321', '3223', 'M');
+INSERT INTO `pessoa` (`id_pessoa`, `nome`, `endereco`, `cpf`, `telefone`, `sexo`) VALUES ('3', 'Agatha', 'rua de barro', '323', '3223', 'F');
+
+INSERT INTO `produto` (`id_produto`, `descricao`, `garantia`, `tipo`, `preco_produto`) VALUES ('1', 'HD', '2017.11.01', 'hardware', '300');
+INSERT INTO `produto` (`id_produto`, `descricao`, `garantia`, `tipo`, `preco_produto`) VALUES ('2', 'SSD', '2017.11.01', 'hardware', '500');
+INSERT INTO `produto` (`id_produto`, `descricao`, `garantia`, `tipo`, `preco_produto`) VALUES ('3', 'GPU', '2017.11.01', 'hardware', '1000');
+
+INSERT INTO `itens_pedido` (`itens_pedido`, `quantidade`, `id_produto`) VALUES ('1', '10', '1');
+INSERT INTO `itens_pedido` (`itens_pedido`, `quantidade`, `id_produto`) VALUES ('2', '20', '2');
+INSERT INTO `itens_pedido` (`itens_pedido`, `quantidade`, `id_produto`) VALUES ('3', '5', '3');
 										
