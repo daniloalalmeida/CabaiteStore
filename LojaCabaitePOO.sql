a153132f0e8ff47c9fb06b1d4f2ac0cc3d7d743a
daniloalalmeida <daniloalalmeida@gmail.com>
2017-11-09 20:19:44 -0200

    Update LojaCabaitePOO.sql

--- LojaCabaitePOO.sql
+++ LojaCabaitePOO.sql
@@ -10,26 +10,29 @@ CREATE TABLE pessoa
     						sexo enum('F', 'M') NOT null);
 					
 CREATE	TABLE produto
-	(id_produto integer(6) PRIMARY KEY AUTO_INCREMENT,
-     		descricao varchar(300) NOT null,
-     			garantia datetime NOT null,
-     				tipo varchar(50) NOT null,
-     					preco_unitario decimal(6,2) NOT null);			
-          
+	(id_produto integer(6) PRIMARY KEY AUTO_INCREMENT, 
+     	descricao varchar(300) NOT null, 
+     		preco_unitario decimal(6,2) NOT null);			
+		
 CREATE TABLE pedido
      (numero_pedido integer(6) PRIMARY KEY,
       	NF integer(6) AUTO_INCREMENT UNIQUE,
-      		data_pedido datetime NOT null,
+      		data_pedido date NOT null,
       			id_pessoa integer(6),
       				CONSTRAINT FK_id_pessoa FOREIGN KEY (id_pessoa) 								
 					REFERENCES pessoa (id_pessoa));
 					
 CREATE TABLE itens_pedido
-	(numero_pedido integer(6), 
-	 	id_produto integer(6),
-	 		quantidade decimal(6,2) unsigned NOT null,
-	 			CONSTRAINT FK_numero_pedido FOREIGN KEY (numero_pedido) REFERENCES pedido (numero_pedido),
-					CONSTRAINT FK_id_produto FOREIGN KEY (id_produto) REFERENCES produto (id_produto));
+	(numero_pedido integer(6),
+     		id_produto integer(6), 
+     			preco_unitario decimal(6,2),
+	 				descricao varchar(300),
+     						quantidade integer(6) unsigned NOT null, 
+     
+     	CONSTRAINT PK_num_id_pedido PRIMARY KEY (numero_pedido, id_produto),    
+     		CONSTRAINT FK_numero_pedido FOREIGN KEY (numero_pedido) REFERENCES pedido (numero_pedido), 
+     			CONSTRAINT FK_id_produto FOREIGN KEY (id_produto) REFERENCES produto (id_produto));
+     				
 
 #DADOS
 
@@ -37,8 +40,8 @@ INSERT INTO `pessoa` (`id_pessoa`, `nome`, `endereco`, `cpf`, `telefone`, `sexo`
 INSERT INTO `pessoa` (`id_pessoa`, `nome`, `endereco`, `cpf`, `telefone`, `sexo`) VALUES ('2', 'Patrick', 'rua de barro', '321', '3223', 'M');
 INSERT INTO `pessoa` (`id_pessoa`, `nome`, `endereco`, `cpf`, `telefone`, `sexo`) VALUES ('3', 'Agatha', 'rua de barro', '323', '3223', 'F');
 
-INSERT INTO `produto` (`id_produto`, `descricao`, `garantia`, `tipo`, `preco_unitario`) VALUES ('1', 'HD', '2017.11.01', 'hardware', '300');
-INSERT INTO `produto` (`id_produto`, `descricao`, `garantia`, `tipo`, `preco_unitario`) VALUES ('2', 'SSD', '2017.11.01', 'hardware', '500');
-INSERT INTO `produto` (`id_produto`, `descricao`, `garantia`, `tipo`, `preco_unitario`) VALUES ('3', 'GPU', '2017.11.01', 'hardware', '1000');
+INSERT INTO `produto` (`id_produto`, `descricao`, `preco_unitario`) VALUES ('1', 'HD', 300);
+INSERT INTO `produto` (`id_produto`, `descricao`, `preco_unitario`) VALUES ('2', 'SSD', 500);
+INSERT INTO `produto` (`id_produto`, `descricao`, `preco_unitario`) VALUES ('3', 'GPU', 1000);
 
 
